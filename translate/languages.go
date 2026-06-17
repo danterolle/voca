381a35e310d86f9064991eff38748e12a6dc92c6
Dario Camonita <danterolle@parrotsec.org>
2026-06-17 15:54:19 +0200

    feat(translate): add StaticLanguages implementing LanguageProvider

--- translate/languages.go
+++ translate/languages.go
@@ -1,5 +1,7 @@
 package translate
 
+import "sort"
+
 const DefaultModel = "gemma4:e2b-it-qat"
 
 var Languages = map[string]string{
@@ -28,4 +30,37 @@ var Languages = map[string]string{
 	"vi":   "Vietnamese",
 	"th":   "Thai",
 	"hi":   "Hindi",
+}
+
+type StaticLanguages struct {
+	codes []string
+	names map[string]string
+}
+
+func NewStaticLanguages() *StaticLanguages {
+	codes := make([]string, 0, len(Languages))
+	for code := range Languages {
+		codes = append(codes, code)
+	}
+	sort.Strings(codes)
+	return &StaticLanguages{
+		codes: codes,
+		names: Languages,
+	}
+}
+
+func (s *StaticLanguages) List() []Language {
+	result := make([]Language, len(s.codes))
+	for i, code := range s.codes {
+		result[i] = Language{Code: code, Name: s.names[code]}
+	}
+	return result
+}
+
+func (s *StaticLanguages) Lookup(code string) (Language, bool) {
+	name, ok := s.names[code]
+	if !ok {
+		return Language{}, false
+	}
+	return Language{Code: code, Name: name}, true
 }
