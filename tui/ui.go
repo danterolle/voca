04aedc399b38dd127316c9581a3db3c1895d6e23
Dario Camonita <danterolle@parrotsec.org>
2026-06-18 18:53:42 +0200

    fix: add defer recover() in BubbleTeaUI.Run to prevent broken terminal on panic

--- tui/ui.go
+++ tui/ui.go
@@ -2,6 +2,8 @@ package tui
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 
@@ -15,6 +17,13 @@ func NewBubbleTeaUI() *BubbleTeaUI {
 }
 
 func (u *BubbleTeaUI) Run(ctx context.Context, core *translate.Core) error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(os.Stderr, "  ✖ panic: %v\n", r)
+			os.Exit(1)
+		}
+	}()
+
 	m := newModel(core)
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
